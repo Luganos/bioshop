f844cf30dd869811c37c638ad74d86aec89e4171
Merecoder <ponomarev.link@gmail.com>
2016-10-31 12:21:38 +0200

    changed one from existing shipping method

--- admin/language/russian/shipping/free.php
+++ admin/language/russian/shipping/free.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']    = 'Бесплатная доставка';
+$_['heading_title']    = 'Курьером предприятия по Днепру бесплатно';
 
 // Text
 $_['text_shipping']    = 'Доставка';
